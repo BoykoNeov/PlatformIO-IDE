332cef7e97c97e40a7b1ddc0d0b016317075edff
BoykoNeov <boikoneov@gmail.com>
2019-11-10 21:01:40 +0200

    :peach: added sk6812 neopixelbus example

---
+++ ESP32/led_compare/src/hw.h
@@ -0,0 +1,4 @@
+// Connect theWS2812B LED strip here
+#define LED_PIN 4
+
+#define LED_COUNT 30
---
+++ ESP32/led_compare/src/main.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#include "hw.h"
+
+#include <NeoPixelBrightnessBus.h>
+NeoPixelBrightnessBus<NeoGrbwFeature, NeoEsp32Rmt0Sk6812Method> strip(LED_COUNT, LED_PIN);
+
+void setup() {
+    Serial.begin(115200);
+
+    strip.Begin();
+    strip.SetBrightness(255);
+    strip.Show();
+}
+
+RgbColor green(0, 0x80, 0);
+RgbColor black(0);
+RgbwColor p;
+
+void loop() 
+{
+    strip.ClearTo(black);
+    strip.Show();
+    delay(100);
+
+    uint32_t startOfCycle;
+    uint32_t endOfCycle;
+
+    startOfCycle = micros();
+    for (uint8_t i = 0; i < LED_COUNT; i++)
+    {
+        strip.SetPixelColor(i, {255, 0, 0, 0});
+    }
+    strip.Show();
+    endOfCycle = micros();
+
+    // Display time in micros of one cycle
+    Serial.println(endOfCycle - startOfCycle);
+
+    delay(2000);
+
+    for (uint8_t i = 0; i < LED_COUNT; i++)
+    {
+        strip.SetPixelColor(i, {0, 255, 0, 0});
+    }
+    strip.Show();
+    delay(2000);
+
+    for (uint8_t i = 0; i < LED_COUNT; i++)
+    {
+        strip.SetPixelColor(i, {0, 0, 255, 0});
+    }
+    strip.Show();
+   delay(2000);
+
+    for (uint8_t i = 0; i < LED_COUNT; i++)
+    {
+        strip.SetPixelColor(i, {0, 0, 0, 255});
+    }
+    strip.Show();
+   delay(2000);
+
+    for (uint8_t i = 0; i < LED_COUNT; i++)
+    {
+        strip.SetPixelColor(i, {255, 255, 255, 255});
+    }
+    strip.Show();
+   delay(2000);
+
+    strip.ClearTo(black);
+    strip.Show();
+
+    // Print RAM usage
+    Serial.println(ESP.getFreeHeap());
+}
